25d6864e5c91926620cfbb3246831c7b27a6a9e3
unknown <bereketlemma4@gmail.com>
2023-09-24 16:31:59 +0300

    created a song dashboard ui

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -71,8 +71,8 @@ const LeftSidebar = () => {
 
 const RighSidebar = () => {
   return (
-    <div className="w-64 h-auto bg-zinc-800 text-zinc-200 flex justify-center items-center p-3 absolute right-0 rounded-b-lg">
-      <div className="w-full h-full bg-slate-00 flex flex-col justify-start items-center p-2">
+    <div className="w-64 h-full bg-zinc-800 text-zinc-200 flex justify-center items-center p-3 absolute right-0 top-0">
+      <div className="w-full h-full bg-slate-00 flex flex-col justify-start items-center pt-20">
         <div className="w-full h-24 flex justify-center items-center py-2 ">
           <div className="w-48 h-full rounded-md flex flex-row justify-center items-center space-x-2 px-3  bg-sky-600">
             <h1 className="text-4xl font-bold">12</h1>
