889fd5363966c1f8011f6d232b2aa201fe27dca8
unknown <bereketlemma4@gmail.com>
2023-09-27 21:26:08 +0300

    added 404 page

---
+++ frontend/src/views/Body/NotFound.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center ">
+      <h1 className="text-sky-500 text-3xl">
+        The page you are looking for is not found.
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
