826c0d8a257afcd887758cd8ec165afe1dcce57c
unknown <bereketlemma4@gmail.com>
2023-09-27 21:27:21 +0300

    updated routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,22 @@ import {
   Outlet,
 } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
+import Artists from "./views/Body/Artists";
+import Songs from "./views/Body/Songs";
+import Albums from "./views/Body/Albums";
+import Genres from "./views/Body/Genres";
+import NotFound from "./views/Body/NotFound";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Outlet />}>
-          <Route path="" element={<MainLayout />} />
-          <Route path="artists" element={<></>} />
-          <Route path="albums" element={<></>} />
-          <Route path="genres" element={<></>} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="" element={<Songs />} />
+          <Route path="artists" element={<Artists />} />
+          <Route path="albums" element={<Albums />} />
+          <Route path="genres" element={<Genres />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </Router>
