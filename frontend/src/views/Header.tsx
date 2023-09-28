ac8e794221d3456b3757020e3ac84b0274ed106b
unknown <bereketlemma4@gmail.com>
2023-09-28 23:35:14 +0300

    dispatch sample action to check backend res

--- frontend/src/views/Header.tsx
+++ frontend/src/views/Header.tsx
@@ -1,4 +1,11 @@
+import { useDispatch } from "react-redux";
+
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch({ type: "song/addSongPending" });
+  };
   return (
     <div className="w-full h-20 bg-zinc-800 text-sky-600 flex flex-row justify-start items-center absolute z-20 right-0 border-b-2 border-zinc-700">
       <div className="w-64 h-full bg-slate-00 flex justify-start items-center ml-3 px-5">
@@ -8,7 +15,10 @@ const Header = () => {
         <h1 className="text-xl font-bold">All Songs</h1>
       </div>
       <div className="w-52 h-full bg-slate-00 flex justify-center items-center absolute right-0 p-5 mr-3">
-        <button className="w-48 h-full outline outline-sky-600 rounded-md">
+        <button
+          onClick={handleClick}
+          className="w-48 h-full outline outline-sky-600 rounded-md"
+        >
           Add Song
         </button>
       </div>
