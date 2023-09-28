04fc3c6375ee5c6ce7c5bba2b29204ecb5e47f2a
unknown <bereketlemma4@gmail.com>
2023-09-28 21:58:31 +0300

    added song fetch dispatch on mount

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,7 +1,14 @@
 import Header from "../views/Header";
 import Body from "../views/Body/Body";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const MainLayout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: "song/fetchSongPending" });
+  }, [dispatch]);
   return (
     <div className="relative">
       <Header />
