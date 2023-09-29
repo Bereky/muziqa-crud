06f8c7c75b8fe7b4c841f4161959371447a934ac
unknown <bereketlemma4@gmail.com>
2023-09-29 23:04:19 +0300

    added api to add song

--- frontend/src/app/apis.tsx
+++ frontend/src/app/apis.tsx
@@ -1,17 +1,39 @@
 import axios from "axios";
+import { PayloadAction } from "@reduxjs/toolkit";
+
+const API_URL = "http://localhost:8400";
+
+interface Song {
+  id: string;
+  title: string;
+  artist: string;
+  genre: string;
+  album: string;
+}
 
 const getSongs = async () => {
-  const response = await axios.get("get-accounts");
+  const response = await axios.get(API_URL + "/api/get-song");
 
   if (response.data) {
-    localStorage.setItem("userAccounts", JSON.stringify(response.data));
+    localStorage.setItem("songs", JSON.stringify(response.data));
   }
 
-  return response.data;
+  return response;
+};
+
+const addNewSong = async (song: Song) => {
+  const response = await axios.post(API_URL + "/api/add-song");
+
+  if (response.data) {
+    localStorage.setItem("songs", JSON.stringify(response.data));
+  }
+
+  return response;
 };
 
 const api = {
   getSongs,
+  addNewSong,
 };
 
 export default api;
