aa9e8100b8c17b228899adcf1131817e99cc794f
unknown <bereketlemma4@gmail.com>
2023-10-01 18:37:08 +0300

    added overall stats on right sidebar

--- frontend/src/views/Sidebar/RightSidebar.tsx
+++ frontend/src/views/Sidebar/RightSidebar.tsx
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
+
 const RighSidebar = () => {
+  const { songs, artists, albums, genres } = useSelector(
+    (state: RootState) => state.song
+  );
+
   return (
     <div className="w-64 h-full bg-zinc-800 text-zinc-200 flex justify-center items-center p-3 absolute right-0 top-0">
       <div className="w-full h-full bg-slate-00 flex flex-col justify-start items-center pt-20">
         <div className="w-full h-24 flex justify-center items-center py-2 ">
           <div className="w-48 h-full rounded-md flex flex-row justify-center items-center space-x-2 px-3  bg-sky-600">
-            <h1 className="text-4xl font-bold">12</h1>
+            <h1 className="text-4xl font-bold">{songs.length}</h1>
             <h2 className="text-lg text-end">Songs</h2>
 
             <i className="fas fa-music text-3xl pl-5"></i>
@@ -13,7 +19,7 @@ const RighSidebar = () => {
 
         <div className="w-full h-24 flex justify-center items-center py-2">
           <div className="w-48 h-full rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
-            <h1 className="text-4xl font-bold">4</h1>
+            <h1 className="text-4xl font-bold">{artists.length}</h1>
             <h2 className="text-lg text-end">Artists</h2>
 
             <i className="fas fa-music text-3xl pl-5"></i>
@@ -22,7 +28,7 @@ const RighSidebar = () => {
 
         <div className="w-full h-24 flex justify-center items-center py-2">
           <div className="w-48 h-full rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
-            <h1 className="text-4xl font-bold">2</h1>
+            <h1 className="text-4xl font-bold">{albums.length}</h1>
             <h2 className="text-lg text-end">Albums</h2>
 
             <i className="fas fa-music text-3xl pl-5"></i>
@@ -31,7 +37,7 @@ const RighSidebar = () => {
 
         <div className="w-full h-24 flex justify-center items-center py-2">
           <div className="w-48 h-full rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
-            <h1 className="text-4xl font-bold">10</h1>
+            <h1 className="text-4xl font-bold">{genres.length}</h1>
             <h2 className="text-lg text-end">Genres</h2>
 
             <i className="fas fa-music text-3xl pl-5"></i>
