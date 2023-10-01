391fb2157e894367c61218d068949bbba10f7d20
unknown <bereketlemma4@gmail.com>
2023-10-01 18:41:08 +0300

    finalized genres stats

--- frontend/src/views/Body/Genres.tsx
+++ frontend/src/views/Body/Genres.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+
 const Genres = () => {
   return (
     <>
@@ -7,50 +9,28 @@ const Genres = () => {
 };
 
 const Table = () => {
+  const { genres } = useSelector((state: RootState) => state.song);
+
   return (
     <table className="min-w-full bg-zin-800 border-none text-zinc-200">
       <thead>
         <tr>
           <th className="py-1 px-1 w-5 text-start">#</th>
-          <th className="py-2 px-4 w-40 text-start">Name</th>
+          <th className="py-2 px-4 w-56 text-start">Name</th>
           <th className="py-2 px-4 w-48 text-start"># of Songs</th>
-          <th className="py-2 px-4 w-32 text-start">Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td className="py-1 px-1 ">1</td>
-          <td className="py-2 px-4 ">Pop</td>
-          <td className="py-2 px-4 ">-</td>
-          <td className="py-2 px-4 ">
-            <div className="flex justify-start items-center flex-row space-x-4">
-              <button className="w-8 h-8 outline outline-slate-500 rounded-md hover:bg-slate-500">
-                <i className="fas fa-pen-to-square text-sm text-sky-400"></i>
-              </button>
-              <button className="w-8 h-8 outline outline-slate-500 rounded-md text-center hover:bg-slate-500">
-                <i className="fas fa-trash text-sm text-red-500"></i>
-              </button>
-            </div>
-          </td>
-        </tr>
-
-        <tr>
-          <td className="py-1 px-1 ">2</td>
-          <td className="py-2 px-4 ">Electronic</td>
-          <td className="py-2 px-4 ">-</td>
-        </tr>
-
-        <tr>
-          <td className="py-1 px-1 ">3</td>
-          <td className="py-2 px-4 ">Soul</td>
-          <td className="py-2 px-4 ">-</td>
-        </tr>
-
-        <tr>
-          <td className="py-1 px-1 ">4</td>
-          <td className="py-2 px-4 ">Jazz</td>
-          <td className="py-2 px-4 ">-</td>
-        </tr>
+        {genres &&
+          genres.map((data, index) => {
+            return (
+              <tr>
+                <td className="py-1 px-1 ">{index + 1}</td>
+                <td className="py-2 px-4 ">{data.genre}</td>
+                <td className="py-2 px-4 ">{data.songs.length}</td>
+              </tr>
+            );
+          })}
       </tbody>
     </table>
   );
