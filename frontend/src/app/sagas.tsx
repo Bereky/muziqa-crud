ded824782c98a92b2de236a8aba79bcbaaa3e799
unknown <bereketlemma4@gmail.com>
2023-10-01 18:42:24 +0300

    finalized my sagas and workers

--- frontend/src/app/sagas.tsx
+++ frontend/src/app/sagas.tsx
@@ -1,60 +1,58 @@
-import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
-import { PayloadAction } from "@reduxjs/toolkit";
+import { call, put, takeEvery } from "redux-saga/effects";
 import api from "./apis";
 
 import {
   fetchSongSuccess,
-  fetchSongPending,
   fetchSongFailure,
-  addSongPending,
   addSongSuccess,
   addSongFailure,
+  updateSongFailure,
+  updateSongSuccess,
+  deleteSongFailure,
+  deleteSongSuccess,
 } from "./songSlice";
-
-interface Song {
-  id: string;
-  title: string;
-  artist: string;
-  genre: string;
-  album: string;
-}
-
-interface FetchSongsResponse {
-  data: Song[];
-}
-
-interface AddSongsResponse {
-  data: Song[];
-}
-
-interface AddSongAction {
-  payload: Song;
-}
 
 function* fetchSongs() {
   try {
-    const response: FetchSongsResponse = yield call(api.getSongs);
+    const response: Response = yield call(api.getSongs);
     yield put(fetchSongSuccess(response.data));
   } catch (error) {
     yield put(fetchSongFailure());
   }
 }
 
-function* addSong(action: { type: string; payload: Song }) {
+function* addSong(action: { type: Object; payload: Song }) {
   try {
-    const response: AddSongsResponse = yield call(
-      api.addNewSong,
-      action.payload
-    );
+    const response: Response = yield call(api.addNewSong, action.payload);
     yield put(addSongSuccess(response.data));
   } catch (error) {
     yield put(addSongFailure());
   }
 }
 
+function* updateSong(action: { type: Object; payload: Song }) {
+  try {
+    const response: Response = yield call(api.updateMySong, action.payload);
+    yield put(updateSongSuccess(response.data));
+  } catch (error) {
+    yield put(updateSongFailure());
+  }
+}
+
+function* deleteSong(action: { type: Object; payload: string }) {
+  try {
+    const response: Response = yield call(api.deleteMySong, action.payload);
+    yield put(deleteSongSuccess(response.data));
+  } catch (error) {
+    yield put(deleteSongFailure());
+  }
+}
+
 function* songSaga() {
   yield takeEvery("song/fetchSongPending", fetchSongs);
   yield takeEvery("song/addSongPending", addSong);
+  yield takeEvery("song/updateSongPending", updateSong);
+  yield takeEvery("song/deleteSongPending", deleteSong);
 }
 
 export default songSaga;
