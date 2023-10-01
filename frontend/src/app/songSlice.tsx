0ead23e5a0babf330cfb5b25977d6a6b5850612c
unknown <bereketlemma4@gmail.com>
2023-10-01 18:44:22 +0300

    finalized song slice

--- frontend/src/app/songSlice.tsx
+++ frontend/src/app/songSlice.tsx
@@ -1,7 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface Song {
+  _id: string;
+  title: string;
+  artist: string;
+  genre: string;
+  album: string;
+}
+
 const initialState = {
   songs: [],
+  artists: [],
+  albums: [],
+  genres: [],
+  songForAction: {},
   status: {
     isError: false,
     isSuccess: false,
@@ -14,32 +26,71 @@ export const songSlice = createSlice({
   name: "song",
   initialState,
   reducers: {
+    resetStatus: (state) => {
+      state.status.isLoading = false;
+      state.status.isError = false;
+      state.status.isSuccess = false;
+    },
+    setSongForAction: (state, action) => {
+      state.songForAction = state.songs.filter(
+        (song: Song) => song._id === action.payload
+      );
+    },
     fetchSongPending: (state) => {
       state.status.isLoading = true;
     },
     fetchSongSuccess: (state, action) => {
-      state.songs = action.payload;
       state.status.isLoading = false;
-      state.status.isSuccess = true;
+      state.status.isError = false;
+      state.status.isSuccess = false;
+      state.songs = action.payload.songs;
+      state.artists = action.payload.artists;
+      state.albums = action.payload.albums;
+      state.genres = action.payload.genres;
       console.log(action.payload);
     },
     fetchSongFailure: (state) => {
-      state.songs = [];
       state.status.isError = true;
       state.status.isSuccess = false;
     },
 
-    addSongPending: (state) => {
+    addSongPending: (state, action) => {
       state.status.isLoading = true;
     },
-    addSongSuccess: (state) => {
-      //state.songs = action.payload;
+    addSongSuccess: (state, action) => {
+      state.songs = action.payload;
       state.status.isLoading = false;
+      state.status.isError = false;
       state.status.isSuccess = true;
-      // console.log(action.payload);
+      console.log(action.payload);
     },
     addSongFailure: (state) => {
-      state.songs = [];
+      state.status.isError = true;
+      state.status.isSuccess = false;
+    },
+    updateSongPending: (state, action) => {
+      state.status.isLoading = true;
+    },
+    updateSongSuccess: (state, action) => {
+      state.songs = action.payload;
+      state.status.isLoading = false;
+      state.status.isError = false;
+      state.status.isSuccess = true;
+    },
+    updateSongFailure: (state) => {
+      state.status.isError = true;
+      state.status.isSuccess = false;
+    },
+    deleteSongPending: (state, action) => {
+      state.status.isLoading = true;
+    },
+    deleteSongSuccess: (state, action) => {
+      state.songs = action.payload;
+      state.status.isLoading = false;
+      state.status.isError = false;
+      state.status.isSuccess = true;
+    },
+    deleteSongFailure: (state) => {
       state.status.isError = true;
       state.status.isSuccess = false;
     },
@@ -47,11 +98,19 @@ export const songSlice = createSlice({
 });
 
 export const {
+  setSongForAction,
   fetchSongPending,
   fetchSongSuccess,
   fetchSongFailure,
   addSongPending,
   addSongSuccess,
   addSongFailure,
+  updateSongPending,
+  updateSongSuccess,
+  updateSongFailure,
+  deleteSongPending,
+  deleteSongSuccess,
+  deleteSongFailure,
+  resetStatus,
 } = songSlice.actions;
 export default songSlice.reducer;
