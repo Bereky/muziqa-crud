a8d5b389d62747c9e24b320a727b73a04666ef80
unknown <bereketlemma4@gmail.com>
2023-10-02 12:41:00 +0300

    added cancel handler to reset form on cancel

--- frontend/src/views/Modals/UpdateSongModal.tsx
+++ frontend/src/views/Modals/UpdateSongModal.tsx
@@ -11,6 +11,7 @@ const UpdateSongModal = ({ message, onClickCB }: ChildProps) => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
 
   const { songForAction } = useSelector((state: RootState) => state.song);
@@ -24,6 +25,12 @@ const UpdateSongModal = ({ message, onClickCB }: ChildProps) => {
     };
 
     dispatch({ type: "song/updateSongPending", payload: updatedSong });
+    reset();
+  };
+
+  const cancelUpdateHandler = () => {
+    onClickCB();
+    reset();
   };
 
   return (
@@ -103,7 +110,7 @@ const UpdateSongModal = ({ message, onClickCB }: ChildProps) => {
                       <button
                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                         type="button"
-                        onClick={onClickCB}
+                        onClick={cancelUpdateHandler}
                       >
                         Cancel
                       </button>
