00a6c712fab4b67bd77520a7489e18fbce4ca262
unknown <bereketlemma4@gmail.com>
2023-10-02 13:11:24 +0300

    added path to frontend build

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const connectToDB = require("./config/database");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8400;
@@ -12,6 +13,12 @@ connectToDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use("/api", router);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log("Server running on port:", PORT));
