c1407f5fb1dab5768ae22565a129a4a61c82c224
unknown <bereketlemma4@gmail.com>
2023-10-02 12:39:22 +0300

    added header titles for dt routes

--- frontend/src/views/Header/Header.tsx
+++ frontend/src/views/Header/Header.tsx
@@ -1,6 +1,9 @@
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <div className="w-full h-20 bg-zinc-800 text-sky-600 flex flex-row justify-start items-center absolute z-20 right-0 border-b-2 border-zinc-700">
       <div className="w-64 h-full bg-slate-00 flex justify-start items-center ml-3 px-5">
@@ -8,7 +11,18 @@ const Header = () => {
       </div>
 
       <div className="w-64 h-full flex justify-start items-center p-0">
-        <h1 className="text-xl font-bold">All Songs</h1>
+        {location.pathname === "/" && (
+          <h1 className="text-xl font-bold"> All Songs</h1>
+        )}
+        {location.pathname === "/artists" && (
+          <h1 className="text-xl font-bold"> All Artists</h1>
+        )}
+        {location.pathname === "/albums" && (
+          <h1 className="text-xl font-bold"> All Albums</h1>
+        )}
+        {location.pathname === "/genres" && (
+          <h1 className="text-xl font-bold"> All Genres</h1>
+        )}
       </div>
       <Button />
     </div>
