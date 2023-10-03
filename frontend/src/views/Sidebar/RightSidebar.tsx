dd607971ff4159e7b5676344867842437789df10
unknown <bereketlemma4@gmail.com>
2023-10-03 09:33:13 +0300

    Bug fix - added conditionals b4 render

--- frontend/src/views/Sidebar/RightSidebar.tsx
+++ frontend/src/views/Sidebar/RightSidebar.tsx
@@ -6,45 +6,50 @@ const RighSidebar = () => {
   );
 
   return (
-    <div className="w-64 h-full min-h-96 bg-zinc-800 text-zinc-200 flex justify-center items-center fixed right-0 pt-2">
-      <div className="w-full h-full bg-zinc-800 flex flex-col justify-start items-center pt-4 ">
-        <div className="w-64 h-auto flex justify-center items-center py-2 ">
-          <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3  bg-sky-600">
-            <h1 className="text-4xl font-bold">{songs.length}</h1>
-            <h2 className="text-lg text-end">Songs</h2>
+    <>
+      {" "}
+      {songs && artists && albums && genres && (
+        <div className="w-64 h-full min-h-96 bg-zinc-800 text-zinc-200 flex justify-center items-center fixed right-0 pt-2">
+          <div className="w-full h-full bg-zinc-800 flex flex-col justify-start items-center pt-4 ">
+            <div className="w-64 h-auto flex justify-center items-center py-2 ">
+              <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3  bg-sky-600">
+                <h1 className="text-4xl font-bold">{songs.length}</h1>
+                <h2 className="text-lg text-end">Songs</h2>
 
-            <i className="fas fa-music text-3xl pl-5"></i>
+                <i className="fas fa-music text-3xl pl-5"></i>
+              </div>
+            </div>
+
+            <div className="w-full h-auto flex justify-center items-center py-2">
+              <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
+                <h1 className="text-4xl font-bold">{artists.length}</h1>
+                <h2 className="text-lg text-end">Artists</h2>
+
+                <i className="fas fa-user text-3xl pl-5"></i>
+              </div>
+            </div>
+
+            <div className="w-full h-auto flex justify-center items-center py-2">
+              <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
+                <h1 className="text-4xl font-bold">{albums.length}</h1>
+                <h2 className="text-lg text-end">Albums</h2>
+
+                <i className="fas fa-compact-disc text-3xl pl-5"></i>
+              </div>
+            </div>
+
+            <div className="w-full h-auto flex justify-center items-center py-2">
+              <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
+                <h1 className="text-4xl font-bold">{genres.length}</h1>
+                <h2 className="text-lg text-end">Genres</h2>
+
+                <i className="fas fa-layer-group text-3xl pl-5"></i>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className="w-full h-auto flex justify-center items-center py-2">
-          <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
-            <h1 className="text-4xl font-bold">{artists.length}</h1>
-            <h2 className="text-lg text-end">Artists</h2>
-
-            <i className="fas fa-user text-3xl pl-5"></i>
-          </div>
-        </div>
-
-        <div className="w-full h-auto flex justify-center items-center py-2">
-          <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
-            <h1 className="text-4xl font-bold">{albums.length}</h1>
-            <h2 className="text-lg text-end">Albums</h2>
-
-            <i className="fas fa-compact-disc text-3xl pl-5"></i>
-          </div>
-        </div>
-
-        <div className="w-full h-auto flex justify-center items-center py-2">
-          <div className="w-48 h-20 rounded-md flex flex-row justify-center items-center space-x-2 px-3 bg-sky-600">
-            <h1 className="text-4xl font-bold">{genres.length}</h1>
-            <h2 className="text-lg text-end">Genres</h2>
-
-            <i className="fas fa-layer-group text-3xl pl-5"></i>
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
